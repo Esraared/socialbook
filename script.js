61f0652755830aef92d362ea7ea36123e2fa81f5
Esraared <147614158+Esraared@users.noreply.github.com>
2024-01-14 01:31:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var settingsmenu = document.querySelector(".setting-menu");
+var darkBtn = document.getElementById("dark-btn")
+
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("setting-menu-height");
+}
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+    if(localStorage.getItem("theme") == "ligth"){
+        localStorage.setItem("theme", "dark");
+    }else{
+        localStorage.setItem("theme", "ligth");
+    }
+}
+
+
+if(localStorage.getItem("theme") == "ligth"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+
+}else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}else{
+    localStorage.setItem("theme", "ligth");
+}
